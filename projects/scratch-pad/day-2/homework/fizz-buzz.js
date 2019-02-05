f9fbb2effbf1118f9650fc386c8d01937a27c3be
Tyler Mott <tyl3rm@gmail.com>
2019-02-05 05:30:15 +0000

    My Third Try

---
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -0,0 +1,31 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * 2: Fizz Buzz
+ *
+ *  a. Write a Function called fizzBuzz that prints the numbers from 1 to 100.
+ *  b. BUT, for multiples of three print “Fizz” instead of the number.
+ *  c. AND for the multiples of five print “Buzz”.
+ *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
+*/
+function fizzBuzz() {
+    // YOUR CODE GOES BELOW HERE //
+for (var i = 1; i <= 100; i++) {
+var x = console.log((i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") ||i);
+}
+return x;
+    // YOUR CODE GOES ABOVE HERE //
+}
+
+
+
+
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+   (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.fizzBuzz = fizzBuzz
+}
